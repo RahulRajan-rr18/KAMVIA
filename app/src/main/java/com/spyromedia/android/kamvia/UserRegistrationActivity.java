d48e50316989670a3689d859683dc1f33e54e578
harithashalu <harithadas341@gmail.com>
2020-05-12 17:00:40 +0530

    code for inserting data to the database

--- app/src/main/java/com/spyromedia/android/kamvia/UserRegistrationActivity.java
+++ app/src/main/java/com/spyromedia/android/kamvia/UserRegistrationActivity.java
@@ -2,11 +2,28 @@ package com.spyromedia.android.kamvia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.spyromedia.android.kamvia.DrawerFragment.MainActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserRegistrationActivity extends AppCompatActivity {
 
@@ -27,9 +44,59 @@ public class UserRegistrationActivity extends AppCompatActivity {
         register_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Register();
             }
         });
 
     }
+
+    public void Register(){
+        String url = "http://192.168.43.132/KAMVIA/login.php";
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    if(!jsonObject.getBoolean("error")){
+
+                        Toast.makeText(UserRegistrationActivity.this, "Registration Successful", Toast.LENGTH_LONG).show();
+                        Intent home = new Intent(UserRegistrationActivity.this, LoginActivity.class);
+                        startActivity(home);
+
+                    }
+                    else{
+
+                        Toast.makeText(UserRegistrationActivity.this, "Registration Failed", Toast.LENGTH_LONG).show();
+
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Toast.makeText(UserRegistrationActivity.this, "Error:"+error.toString(), Toast.LENGTH_SHORT).show();
+
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+                Map<String, String> params = new HashMap<>();
+                params.put("username",first_name.getText().toString().trim());
+                params.put("mob_no",mob_no.getText().toString().trim());
+                params.put("password",password.getText().toString().trim());
+
+                return  params;
+            }
+        };
+        requestQueue.add(stringRequest);
+    }
+
 }
