ac7e875d2d84df80d9f85cd12ba714b0092df716
RahulRajan-rr18 <30716985+RahulRajan-rr18@users.noreply.github.com>
2020-05-14 23:20:38 +0530

    changes in view profile, added autocomplete textview in search pages.

--- app/src/main/java/com/spyromedia/android/kamvia/SearchByLocationFragment.java
+++ app/src/main/java/com/spyromedia/android/kamvia/SearchByLocationFragment.java
@@ -6,8 +6,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,26 +18,32 @@ import androidx.fragment.app.Fragment;
 public class SearchByLocationFragment extends Fragment {
 
     AutoCompleteTextView searchLocation;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search_by_location, container, false);
         searchLocation = view.findViewById(R.id.searchLocation);
 
-        String[] ProgLanguages = { "Java", "C", "C++", ".Net", "PHP", "Perl",
-                "Objective-c", "Small-Talk", "C#", "Ruby", "ASP", "ASP .NET" };
+        String[] itemlistvalues = {"Raju", "Balu", "Raman", "KrishnanKutty", "Gopalan", "Ashok",
+                "Anjas", "Muhammed", "Chandran", "Anoop", "Aravind"};
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(),
-                android.R.layout.select_dialog_item, ProgLanguages);
+                android.R.layout.select_dialog_item, itemlistvalues);
         //Used to specify minimum number of
         //characters the user has to type in order to display the drop down hint.
         searchLocation.setThreshold(1);
         //Setting adapter
         searchLocation.setAdapter(arrayAdapter);
+        searchLocation.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = searchLocation.getText().toString();
+                Toast.makeText(getContext(), "Something Selected"+item, Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return view;
     }
-
-
 
 }
--- app/src/main/java/com/spyromedia/android/kamvia/SearchByNameFragement.java
+++ app/src/main/java/com/spyromedia/android/kamvia/SearchByNameFragement.java
@@ -5,7 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,8 +21,22 @@ public class SearchByNameFragement extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search_by_name, container, false);
 
+        String[] itemlistvalues = {"Raju", "Balu", "Raman", "KrishnanKutty", "Gopalan", "Ashok",
+                "Anjas", "Muhammed", "Chandran", "Anoop", "Aravind"};
 
+        AutoCompleteTextView SearchByname = view.findViewById(R.id.searchbyname);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(),
+                android.R.layout.select_dialog_item, itemlistvalues);
+        SearchByname.setAdapter(arrayAdapter);
 
+        SearchByname.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Toast.makeText(getContext(), "Hola...", Toast.LENGTH_SHORT).show();
+
+            }
+        });
         return view;
     }
 }
