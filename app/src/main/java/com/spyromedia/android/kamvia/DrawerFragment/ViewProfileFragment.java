f43cebfb8209abcbd7870799f93a2c107d7d5ee9
harithashalu <harithadas341@gmail.com>
2020-05-22 16:02:49 +0530

    assign id to view profile fragment

--- app/src/main/java/com/spyromedia/android/kamvia/DrawerFragment/ViewProfileFragment.java
+++ app/src/main/java/com/spyromedia/android/kamvia/DrawerFragment/ViewProfileFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,9 +13,13 @@ import androidx.fragment.app.Fragment;
 import com.spyromedia.android.kamvia.R;
 
 public class ViewProfileFragment extends Fragment {
+
+    TextView user_name,email,employee_no;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_viewprofile,container,false);
+        
     }
 }
