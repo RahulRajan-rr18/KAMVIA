9c6c19deec5ecdd37d787720d279d014c830a672
harithashalu <harithadas341@gmail.com>
2020-05-27 19:47:48 +0530

    AddPost function added

--- app/src/main/java/com/spyromedia/android/kamvia/AddNewPostActivity.java
+++ app/src/main/java/com/spyromedia/android/kamvia/AddNewPostActivity.java
@@ -2,10 +2,31 @@ package com.spyromedia.android.kamvia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AddNewPostActivity extends AppCompatActivity {
 
@@ -28,6 +49,7 @@ public class AddNewPostActivity extends AppCompatActivity {
                 Boolean verify = verifyPost();
 
                 if(verify == true){
+                    AddNewPost();
                     // add file post to database
                 }
             }
@@ -46,6 +68,65 @@ public class AddNewPostActivity extends AppCompatActivity {
             return false;
         }
         return true;
+    }
+
+    public void AddNewPost(){
+
+        String url = "http://18.220.53.162/kamvia/api/addnewpost.php";
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    if (!jsonObject.getBoolean("error")) {
+
+                        Toast.makeText(AddNewPostActivity.this, "Post Added Successfully", Toast.LENGTH_LONG).show();
+
+                    } else {
+
+                        Toast.makeText(AddNewPostActivity.this, "Failed", Toast.LENGTH_LONG).show();
+
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                if (error instanceof NetworkError) {
+                } else if (error instanceof ServerError) {
+
+                    Toast.makeText(AddNewPostActivity.this, "Server Error"+error, Toast.LENGTH_SHORT).show();
+
+                } else if (error instanceof AuthFailureError) {
+                } else if (error instanceof ParseError) {
+                } else if (error instanceof NoConnectionError) {
+                } else if (error instanceof TimeoutError) {
+                    Toast.makeText(AddNewPostActivity.this,
+                            "Oops. Timeout error!",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+                Map<String, String> params = new HashMap<>();
+                params.put("user_id", "123");
+                params.put("post_heading", postHead.getText().toString().trim());
+                params.put("content", postContent.getText().toString().trim());
+
+                return params;
+            }
+        };
+        requestQueue.add(stringRequest);
     }
 
 
