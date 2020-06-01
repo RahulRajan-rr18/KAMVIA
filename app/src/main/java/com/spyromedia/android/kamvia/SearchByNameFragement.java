4fc4db69c43e5df3d451728ffef875ee551ad157
harithashalu <harithadas341@gmail.com>
2020-06-01 11:31:59 +0530

    search byname

--- app/src/main/java/com/spyromedia/android/kamvia/SearchByNameFragement.java
+++ app/src/main/java/com/spyromedia/android/kamvia/SearchByNameFragement.java
@@ -14,20 +14,26 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class SearchByNameFragement extends Fragment {
     RequestQueue requestQueue;
     AutoCompleteTextView SearchByname;
+    private RecyclerView recyclerView;
+    private SearchResultAdapter adapter;
+    private List<SearchResultRecyItem> resultList;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -41,6 +47,10 @@ public class SearchByNameFragement extends Fragment {
                 android.R.layout.select_dialog_item, itemlistvalues);
         SearchByname.setAdapter(arrayAdapter);
 
+        resultList = new ArrayList<>();
+        requestQueue = Volley.newRequestQueue(getContext());
+        ListNames();
+
         SearchByname.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -53,4 +63,42 @@ public class SearchByNameFragement extends Fragment {
         });
         return view;
     }
+
+    private void ListNames(){
+
+        String url = "http://18.220.53.162/kamvia/api/users.php";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("data");
+
+                    for (int i=0; i<jsonArray.length(); i++){
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                        String uname = jsonObject.getString("name");
+                        String location = jsonObject.getString("home_station");
+                        String stationcode = jsonObject.getString("home_station_code");
+
+                        resultList.add(new SearchResultRecyItem(uname,location,stationcode));
+
+                    }
+                    adapter = new SearchResultAdapter(resultList,getActivity());
+                    recyclerView.setAdapter(adapter);
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
+    }
+
 }
