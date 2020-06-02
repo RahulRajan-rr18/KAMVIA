d7fa75e996b479912391a6af2c4cd82c0cff4d5d
harithashalu <harithadas341@gmail.com>
2020-06-02 20:38:39 +0530

    function to check mobile number is registered or not

--- app/src/main/java/com/spyromedia/android/kamvia/ForgotPasswordActivity.java
+++ app/src/main/java/com/spyromedia/android/kamvia/ForgotPasswordActivity.java
@@ -34,7 +34,7 @@ public class ForgotPasswordActivity extends AppCompatActivity {
 TextView error;
 EditText et_Otp , btn_otp , newpassword , confirmPassword , mobile_number ;
 
-Button  btn_reset , btn_otpVerification;
+Button  btn_reset , btn_otpVerification , send_otp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +43,7 @@ Button  btn_reset , btn_otpVerification;
         getSupportActionBar().hide();
 
         mobile_number = findViewById(R.id.mobile_number);
+        send_otp = findViewById(R.id.send_otp);
 
         error = findViewById(R.id.tv_numbernotfound);
         error.setVisibility(View.INVISIBLE);
@@ -64,6 +65,13 @@ Button  btn_reset , btn_otpVerification;
 
         btn_reset = findViewById(R.id.btn_reset);
         btn_reset.setVisibility(View.INVISIBLE);
+
+        send_otp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                VerifyNumber();
+            }
+        });
 
         btn_reset.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -158,6 +166,66 @@ Button  btn_reset , btn_otpVerification;
 
                 Map<String, String> params = new HashMap<>();
                 params.put("password", newpassword.getText().toString().trim());
+                params.put("mobile_number",mobile_number.getText().toString().trim());
+                return params;
+            }
+        };
+        requestQueue.add(stringRequest);
+    }
+
+    public void VerifyNumber(){
+
+        String url = "http://18.220.53.162/kamvia/api/check_number.php";
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+
+
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    if (!jsonObject.getBoolean("error")) {
+
+                        Toast.makeText(ForgotPasswordActivity.this, "Registered using this number", Toast.LENGTH_LONG).show();
+                        // here we can call otp function
+
+                    } else {
+
+                        Toast.makeText(ForgotPasswordActivity.this, "Not registered with this number", Toast.LENGTH_LONG).show();
+
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                if (error instanceof NetworkError) {
+                } else if (error instanceof ServerError) {
+
+                    Toast.makeText(ForgotPasswordActivity.this, "Server Error"+error, Toast.LENGTH_SHORT).show();
+
+                } else if (error instanceof AuthFailureError) {
+                } else if (error instanceof ParseError) {
+                } else if (error instanceof NoConnectionError) {
+                } else if (error instanceof TimeoutError) {
+                    Toast.makeText(ForgotPasswordActivity.this,
+                            "Oops. Timeout error!",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+                Map<String, String> params = new HashMap<>();
                 params.put("mobile_number",mobile_number.getText().toString().trim());
                 return params;
             }
