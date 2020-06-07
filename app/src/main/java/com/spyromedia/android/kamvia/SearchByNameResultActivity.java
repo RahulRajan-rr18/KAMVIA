49183a3d15391f3fa2d40003ab92a2b2967e4754
harithashalu <harithadas341@gmail.com>
2020-06-07 14:50:37 +0530

    result of name search

--- app/src/main/java/com/spyromedia/android/kamvia/SearchByNameResultActivity.java
+++ app/src/main/java/com/spyromedia/android/kamvia/SearchByNameResultActivity.java
@@ -6,11 +6,17 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.ParseError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
@@ -33,9 +39,8 @@ public class SearchByNameResultActivity extends AppCompatActivity {
     private SearchResultAdapter adapter;
     private List<SearchResultRecyItem> resultList;
     String name;
+    ProgressDialog progressDialog;
 
-
-    //TextView ClassName, SpeakerId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,47 +54,51 @@ public class SearchByNameResultActivity extends AppCompatActivity {
         Intent intent = getIntent();
         name = intent.getStringExtra("name");
 
-        // requestQueue = Volley.newRequestQueue(this);
-        parseJSON();
-
+        FetchDetails();
 
     }
 
-    private void parseJSON() {
+    public void FetchDetails(){
 
-        String url = "http://18.220.53.162/kamvia/api/users.php";
+        String url = "http://18.220.53.162/kamvia/api/NameLike.php";
         RequestQueue requestQueue = Volley.newRequestQueue(this);
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, null, new Response.Listener<JSONObject>() {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
             @Override
-            public void onResponse(JSONObject response) {
+            public void onResponse(String response) {
+                progressDialog.dismiss();
                 try {
-                    JSONArray jsonArray = response.getJSONArray("data");
 
-
-                    for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONArray jsonArray = new JSONArray(response);
+                    for (int i = 0; i < jsonArray.length(); i++){
                         JSONObject jsonObject = jsonArray.getJSONObject(i);
-
-                        String uname = jsonObject.getString("name");
-                        String location = jsonObject.getString("home_station");
-                        String stationcode = jsonObject.getString("home_station_code");
-                        String user_id = jsonObject.getString("user_id");
-
-                        resultList.add(new SearchResultRecyItem(user_id, uname, location, stationcode));
+                        String name = jsonObject.getString("name");
                     }
-
-                    adapter = new SearchResultAdapter(resultList, SearchByNameResultActivity.this);
-                    recyclerView.setAdapter(adapter);
 
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
+
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                error.printStackTrace();
+                progressDialog.dismiss();
+                if (error instanceof NetworkError) {
+                } else if (error instanceof ServerError) {
+
+                    Toast.makeText(SearchByNameResultActivity.this, "Server Error"+error, Toast.LENGTH_SHORT).show();
+
+                } else if (error instanceof AuthFailureError) {
+                } else if (error instanceof ParseError) {
+                } else if (error instanceof NoConnectionError) {
+                } else if (error instanceof TimeoutError) {
+                    Toast.makeText(SearchByNameResultActivity.this,
+                            "Oops. Timeout error!",
+                            Toast.LENGTH_LONG).show();
+                }
             }
+
         }) {
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
@@ -97,10 +106,14 @@ public class SearchByNameResultActivity extends AppCompatActivity {
                 Map<String, String> params = new HashMap<>();
                 params.put("name", name.trim());
 
+
                 return params;
             }
         };
-        requestQueue.add(jsonObjectRequest);
+        requestQueue.add(stringRequest);
+        progressDialog = new ProgressDialog(SearchByNameResultActivity.this);
+        progressDialog.setMessage("Loading....");
+        progressDialog.show();
     }
 
 }
