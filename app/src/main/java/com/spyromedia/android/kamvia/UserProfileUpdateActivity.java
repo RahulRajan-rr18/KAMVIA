d017234c9da12e802ee5369198b371b25768b904
RahulRajan-rr18 <30716985+RahulRajan-rr18@users.noreply.github.com>
2020-06-13 00:05:58 +0530

    profile photo updation

--- app/src/main/java/com/spyromedia/android/kamvia/UserProfileUpdateActivity.java
+++ app/src/main/java/com/spyromedia/android/kamvia/UserProfileUpdateActivity.java
@@ -1,23 +1,21 @@
 package com.spyromedia.android.kamvia;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
-import android.Manifest;
 import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
-import android.os.Build;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.provider.Settings;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
@@ -25,7 +23,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
-import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -45,17 +42,29 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
-public class UserProfileUpdateActivity extends AppCompatActivity {
+public class UserProfileUpdateActivity extends AppCompatActivity implements View.OnClickListener {
     final Calendar myCalendar = Calendar.getInstance();
     final Calendar myCalendar2 = Calendar.getInstance();
-    Spinner home_district, present_rto_district,membership_type;
-    ProgressDialog progressDialog , ImageUploadProgressBar;
-    TextView errorDist,errorRtoDist;
-    RadioGroup  member_fee_paid;
+    Spinner home_district, present_rto_district, membership_type;
+    ProgressDialog progressDialog, ImageUploadProgressBar;
+    TextView errorDist, errorRtoDist;
+    RadioGroup member_fee_paid;
     RadioButton radioButton;
-    Button photo_upload , upload_details;
+    Button pickImageButton, upload_detailsButton, uploadImageButton;
     EditText dateofbirth, dateOfJoiningasamvi;
     EditText name, email, employee_number, add_line1, add_line2, pincode, state, home_station_code, present_station_code;
+
+    public static final String UPLOAD_URL = "http://18.220.53.162/kamvia/api/img.php";
+    public static final String UPLOAD_KEY = "image";
+    public static final String TAG = "MY MESSAGE";
+    private int PICK_IMAGE_REQUEST = 1;
+    private Bitmap bitmap;
+    private Uri filePath;
+
+    ImageView profile_image;
+
+
+    boolean check = true;
 
     final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
 
@@ -106,40 +115,22 @@ public class UserProfileUpdateActivity extends AppCompatActivity {
         present_rto_district = findViewById(R.id.id_present_district);
         present_station_code = findViewById(R.id.present_station_code);
         member_fee_paid = findViewById(R.id.rd_group_feepaid);
-        photo_upload = findViewById(R.id.btn_uploadphoto);
-        upload_details = findViewById(R.id.btn_upload);
+        pickImageButton = findViewById(R.id.btn_pickImage);
+        uploadImageButton = findViewById(R.id.btn_upload_image);
+        upload_detailsButton = findViewById(R.id.btn_upload);
         errorDist = findViewById(R.id.error_dist);
         errorRtoDist = findViewById(R.id.error_rto_dist);
         membership_type = findViewById(R.id.id_memtype);
 
-        //upload_details.setAlpha(.5f);
-      //  upload_details.setClickable(false);
-        upload_details.setVisibility(View.INVISIBLE);
+        profile_image = findViewById(R.id.imagepicked);
 
+        pickImageButton.setOnClickListener(this);
+        uploadImageButton.setOnClickListener(this);
 
-
-        photo_upload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(i, 100);
-            }
-
-
-        });
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && ContextCompat.checkSelfPermission(this,
-//                Manifest.permission.READ_EXTERNAL_STORAGE)
-//                != PackageManager.PERMISSION_GRANTED) {
-//            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-//            intent.setType("image/*");
-//            // startActivityForResult(intent, PICK_PHOTO_FOR_AVATAR);
-//            finish();
-//            startActivity(intent);
-//            return;
-//        }
 
         int selectedId = member_fee_paid.getCheckedRadioButtonId();
         radioButton = findViewById(selectedId);
+
 
         dateofbirth.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -162,12 +153,12 @@ public class UserProfileUpdateActivity extends AppCompatActivity {
         });
 
 
-        upload_details.setOnClickListener(new View.OnClickListener() {
+        upload_detailsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Boolean verify = vefifyDetails();
-                if(verify == true){
+                if (verify == true) {
                     Update_Profile();
                 }
             }
@@ -175,131 +166,56 @@ public class UserProfileUpdateActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 100 && resultCode == RESULT_OK && data != null) {
-
-            //getting the image Uri
-            Uri imageUri = data.getData();
-            try {
-                //getting bitmap object from uri
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
-
-                //calling the method uploadBitmap to upload image
-                uploadBitmap(bitmap);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-    public byte[] getFileDataFromDrawable(Bitmap bitmap) {
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.PNG, 50, byteArrayOutputStream);
-        return byteArrayOutputStream.toByteArray();
-    }
-    private void uploadBitmap(final Bitmap bitmap) {
-
-        String URL = "http://18.220.53.162/kamvia/api/image_upload.php";
-
-        //our custom volley request
-        VolleyMultipartRequest volleyMultipartRequest = new VolleyMultipartRequest(Request.Method.POST, URL,
-                new Response.Listener<NetworkResponse>() {
-                    @Override
-                    public void onResponse(NetworkResponse response) {
-                        ImageUploadProgressBar.dismiss();
-                        try {
-                            JSONObject obj = new JSONObject(new String(response.data));
-                            Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
-                            //upload_details.setAlpha(.1f);
-                          // upload_details.setClickable(true);
-                            upload_details.setVisibility(View.VISIBLE);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        ImageUploadProgressBar.dismiss();
-                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                }) {
-
-            /*
-             * Here we are passing image by renaming it with a unique name
-             * */
-            @Override
-            protected Map<String, VolleyMultipartRequest.DataPart> getByteData() {
-                Map<String, VolleyMultipartRequest.DataPart> params = new HashMap<>();
-                long imagename = System.currentTimeMillis();
-                params.put("pic", new VolleyMultipartRequest.DataPart(imagename + ".png", getFileDataFromDrawable(bitmap)));
-                return params;
-            }
-
-        };
-
-        //adding the request to volley
-        Volley.newRequestQueue(this).add(volleyMultipartRequest);
-        ImageUploadProgressBar = new ProgressDialog(UserProfileUpdateActivity.this);
-        ImageUploadProgressBar.setMessage("Uploading Image......");
-        ImageUploadProgressBar.setCancelable(false);
-        ImageUploadProgressBar.show();
-
-    }
-
 
     private Boolean vefifyDetails() {
-        if(name.getText().toString().trim().isEmpty() == true){
+        if (name.getText().toString().trim().isEmpty() == true) {
             name.setError("Name reqired");
-            return  false;
+            return false;
         }
-        if(email.getText().toString().trim().isEmpty() == true){
+        if (email.getText().toString().trim().isEmpty() == true) {
             email.setError("Email reqired");
-            return  false;
+            return false;
         }
-        if(employee_number.getText().toString().trim().isEmpty() == true){
+        if (employee_number.getText().toString().trim().isEmpty() == true) {
             employee_number.setError("PEN reqired");
-            return  false;
+            return false;
         }
-        if(add_line1.getText().toString().trim().isEmpty() == true){
+        if (add_line1.getText().toString().trim().isEmpty() == true) {
             add_line1.setError("Reqired");
-            return  false;
+            return false;
         }
-        if(add_line2.getText().toString().trim().isEmpty() == true){
+        if (add_line2.getText().toString().trim().isEmpty() == true) {
             add_line2.setError("Reqired");
-            return  false;
+            return false;
         }
 
-        if(pincode.getText().toString().trim().isEmpty() == true){
+        if (pincode.getText().toString().trim().isEmpty() == true) {
             pincode.setError("Please enter the pincode");
-            return  false;
+            return false;
         }
-        if(home_station_code.getText().toString().trim().isEmpty() == true){
+        if (home_station_code.getText().toString().trim().isEmpty() == true) {
             home_station_code.setError("Home station code reqired");
-            return  false;
+            return false;
         }
-        if(present_station_code.getText().toString().trim().isEmpty() == true){
+        if (present_station_code.getText().toString().trim().isEmpty() == true) {
             present_station_code.setError("Present station code reqired");
-            return  false;
+            return false;
         }
-        if(dateofbirth.getText().toString().isEmpty()==true){
+        if (dateofbirth.getText().toString().isEmpty() == true) {
             dateofbirth.setText("Reqired");
-            return  false;
+            return false;
         }
-        if(home_district.getSelectedItem().toString().equals("Choose")){
+        if (home_district.getSelectedItem().toString().equals("Choose")) {
             errorDist.setError("Please choose your district");
-            return  false;
+            return false;
         }
 
-        if(present_rto_district.getSelectedItem().toString().equals("Choose")){
+        if (present_rto_district.getSelectedItem().toString().equals("Choose")) {
             errorRtoDist.setError("Please choose current district");
-            return  false;
+            return false;
         }
 
-        return  true;
+        return true;
 
     }
 
@@ -311,12 +227,14 @@ public class UserProfileUpdateActivity extends AppCompatActivity {
         dateofbirth.setText(sdf.format(myCalendar.getTime()));
 
     }
+
     private void updateLabel2() {
         String myFormat = "YYYY/MM/dd"; //In which you need put here
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.ROOT);
         dateOfJoiningasamvi.setText(sdf.format(myCalendar2.getTime()));
 
     }
+
     public void Update_Profile() {
 
         String url = "http://18.220.53.162/kamvia/api/UserProleUpdate.php";
@@ -337,7 +255,7 @@ public class UserProfileUpdateActivity extends AppCompatActivity {
                     if (!jsonObject.getBoolean("error")) {
 
                         Toast.makeText(UserProfileUpdateActivity.this, "Profile Updated", Toast.LENGTH_LONG).show();
-                        Intent home = new Intent(UserProfileUpdateActivity.this , HomeFragment.class);
+                        Intent home = new Intent(UserProfileUpdateActivity.this, HomeFragment.class);
                         startActivity(home);
                         finish();
 
@@ -365,24 +283,24 @@ public class UserProfileUpdateActivity extends AppCompatActivity {
             protected Map<String, String> getParams() throws AuthFailureError {
 
                 Map<String, String> params = new HashMap<>();
-                params.put("user_id",Globals.currentUser.USER_ID);
-                params.put("name",name.getText().toString().trim());
-                params.put("employee_number",employee_number.getText().toString().trim());
-                params.put("email",email.getText().toString().trim());
-                params.put("mobile_number",Globals.currentUser.MOBILE_NUMBER);
-                params.put("date_of_birth",dateofbirth.getText().toString().trim());
-                params.put("address",add_line1.getText().toString().trim() + add_line2.getText().toString().trim());
-                params.put("home_station_code",home_station_code.getText().toString().trim());
-                params.put("home_station",home_station_code.getText().toString().trim());
-                params.put("date_of_joining",dateOfJoiningasamvi.getText().toString().trim());
-                params.put("present_station_code",present_station_code.getText().toString().trim() + add_line2.getText().toString().trim());
-                params.put("present_station",present_station_code.getText().toString().trim());
-                params.put("state","Kerala");
-                params.put("home_pincode",pincode.getText().toString().trim());
-                params.put("home_district",home_district.getSelectedItem().toString().trim());
-                params.put("fee_paid",radioButton.getText().toString().trim());
-                params.put("member_type",membership_type.getSelectedItem().toString().trim());
-                params.put("present_rto_district",present_rto_district.getSelectedItem().toString().trim());
+                params.put("user_id", Globals.currentUser.USER_ID);
+                params.put("name", name.getText().toString().trim());
+                params.put("employee_number", employee_number.getText().toString().trim());
+                params.put("email", email.getText().toString().trim());
+                params.put("mobile_number", Globals.currentUser.MOBILE_NUMBER);
+                params.put("date_of_birth", dateofbirth.getText().toString().trim());
+                params.put("address", add_line1.getText().toString().trim() + add_line2.getText().toString().trim());
+                params.put("home_station_code", home_station_code.getText().toString().trim());
+                params.put("home_station", home_station_code.getText().toString().trim());
+                params.put("date_of_joining", dateOfJoiningasamvi.getText().toString().trim());
+                params.put("present_station_code", present_station_code.getText().toString().trim() + add_line2.getText().toString().trim());
+                params.put("present_station", present_station_code.getText().toString().trim());
+                params.put("state", "Kerala");
+                params.put("home_pincode", pincode.getText().toString().trim());
+                params.put("home_district", home_district.getSelectedItem().toString().trim());
+                params.put("fee_paid", radioButton.getText().toString().trim());
+                params.put("member_type", membership_type.getSelectedItem().toString().trim());
+                params.put("present_rto_district", present_rto_district.getSelectedItem().toString().trim());
                 return params;
             }
         };
@@ -392,4 +310,82 @@ public class UserProfileUpdateActivity extends AppCompatActivity {
         progressDialog.show();
     }
 
+
+    @Override
+    public void onClick(View v) {
+        if (v == pickImageButton) {
+            showFileChooser();
+        }
+        if (v == uploadImageButton) {
+            uploadImage();
+        }
+    }
+
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            filePath = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                profile_image.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getStringImage(Bitmap bmp) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return encodedImage;
+    }
+
+    private void uploadImage() {
+        class UploadImage extends AsyncTask<Bitmap, Void, String> {
+
+            ProgressDialog loading;
+            RequestHandler rh = new RequestHandler();
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(UserProfileUpdateActivity.this, "Uploading Image", "Please wait...", true, true);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            protected String doInBackground(Bitmap... params) {
+                Bitmap bitmap = params[0];
+                String uploadImage = getStringImage(bitmap);
+
+                HashMap<String, String> data = new HashMap<>();
+                data.put(UPLOAD_KEY, uploadImage);
+
+                String result = rh.sendPostRequest(UPLOAD_URL, data);
+
+                return result;
+            }
+
+        }
+        UploadImage ui = new UploadImage();
+        ui.execute(bitmap);
+    }
 }
