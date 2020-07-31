75947c015fc53b46242b826ee1b7f14ce090ed44
patrickprakash <patrickprakash8@gmail.com>
2020-07-31 15:17:33 +0530

    Added the layouts of the OTP Verification

---
+++ app/src/main/java/com/spyromedia/android/kamvia/OtpVerificationActivity.java
@@ -0,0 +1,14 @@
+package com.spyromedia.android.kamvia;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OtpVerificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp_verification);
+    }
+}
