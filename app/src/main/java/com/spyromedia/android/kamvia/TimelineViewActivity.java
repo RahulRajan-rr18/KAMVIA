3906fadc34527a459625e5ab44778abc4bd2f408
RahulRajan-rr18 <30716985+RahulRajan-rr18@users.noreply.github.com>
2020-08-02 23:24:08 +0530

    pdf upload  and show completed

--- app/src/main/java/com/spyromedia/android/kamvia/TimelineViewActivity.java
+++ app/src/main/java/com/spyromedia/android/kamvia/TimelineViewActivity.java
@@ -1,8 +1,13 @@
 package com.spyromedia.android.kamvia;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,6 +18,7 @@ public class TimelineViewActivity extends AppCompatActivity {
     ImageView backarrow;
     TextView tv_heading, tv_condent, tv_link;
     String pdfurl;
+    WebView pdfView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +26,45 @@ public class TimelineViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_timeline_view);
         getSupportActionBar().hide();
 
+
         tv_heading = findViewById(R.id.tv_heading);
         tv_condent = findViewById(R.id.tv_condent);
         pdfurl = getIntent().getStringExtra("pdfurl");
 
-        tv_heading.setText(""+getIntent().getStringExtra("heading"));
-        tv_condent.setText(""+getIntent().getStringExtra("condent"));
+        tv_heading.setText("" + getIntent().getStringExtra("heading"));
+        tv_condent.setText("" + getIntent().getStringExtra("condent"));
+
+        pdfView = (WebView) findViewById(R.id.wvpdf);
+        pdfView.getSettings().setJavaScriptEnabled(true);
+        String filename = pdfurl;
+        pdfView.loadUrl("http://docs.google.com/gview?embedded=true&url=" + filename);
+
+        pdfView.setWebViewClient(new WebViewClient() {
+            public void onPageFinished(WebView view, String url) {
+            }
+        });
+
+        pdfView.setOnTouchListener(new View.OnTouchListener(){
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction()==MotionEvent.ACTION_MOVE){
+                    return false;
+                }
+
+                if (event.getAction()==MotionEvent.ACTION_UP){
+
+                    Uri webpage = Uri.parse(pdfurl);
+                    Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+                    if (intent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(intent);
+                    }
+
+                }
+
+                return false;
+            }
+        });
 
         backarrow = findViewById(R.id.iv_backarrow);
         backarrow.setOnClickListener(new View.OnClickListener() {
