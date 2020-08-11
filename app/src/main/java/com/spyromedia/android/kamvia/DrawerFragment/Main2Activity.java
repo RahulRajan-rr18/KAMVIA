e3e828566fd3c10a5a89d7f7d0dbb8ad41753d25
patrickprakash <patrickprakash8@gmail.com>
2020-08-11 19:28:40 +0530

    Fixed the bug

---
+++ app/src/main/java/com/spyromedia/android/kamvia/DrawerFragment/Main2Activity.java
@@ -0,0 +1,20 @@
+package com.spyromedia.android.kamvia.DrawerFragment;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.spyromedia.android.kamvia.R;
+
+public class Main2Activity extends AppCompatActivity {
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_new_design);
+
+
+    }
+}
