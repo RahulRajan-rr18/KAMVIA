5388848a16d2ce0803ecdd5c6943733823ac3f72
patrickprakash <patrickprakash8@gmail.com>
2020-08-14 16:31:42 +0530

    Added password reset activity

--- app/src/main/java/com/spyromedia/android/kamvia/ForgotPasswordActivity.java
+++ app/src/main/java/com/spyromedia/android/kamvia/ForgotPasswordActivity.java
@@ -1,24 +1,142 @@
 package com.spyromedia.android.kamvia;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ForgotPasswordActivity extends AppCompatActivity {
-TextView error;
-EditText et_Otp , btn_otp , newpassword , confirmPassword , mobile_number ;
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
-Button  btn_reset , btn_otpVerification , send_otp;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+
+
+MaterialButton sendOTP;
+TextInputEditText mobile_number;
+String mobile_number_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+        sendOTP = findViewById(R.id.otpverify_btn);
+        mobile_number = findViewById(R.id.mobilenumber_text);
+       mobile_number_text =  mobile_number.getText().toString();
+        sendOTP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-//        getSupportActionBar().hide();
-//
+                VerifyNumber();;
+
+                finish();
+            }
+        });
+
+
+    }
+
+
+        public void VerifyNumber()
+    {
+
+        String url = "http://18.220.53.162/kamvia/api/CheckNumber.php";
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+
+
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+
+                    Toast.makeText(ForgotPasswordActivity.this, jsonObject.getString("message"), Toast.LENGTH_LONG).show();
+                    if (!jsonObject.getBoolean("error")) {
+
+                        //Intent to next activity
+                        Intent intent = new Intent(ForgotPasswordActivity.this,PasswordResetActivity.class);
+                        intent.putExtra("mobile_number",mobile_number_text);
+                        startActivity(intent);
+
+
+
+                    } else {
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                if (error instanceof NetworkError) {
+                } else if (error instanceof ServerError) {
+
+                    Toast.makeText(ForgotPasswordActivity.this, "Server Error"+error, Toast.LENGTH_SHORT).show();
+
+                } else if (error instanceof AuthFailureError) {
+                } else if (error instanceof ParseError) {
+               } else if (error instanceof NoConnectionError) {
+                } else if (error instanceof TimeoutError) {
+                    Toast.makeText(ForgotPasswordActivity.this,
+                            "Oops. Timeout error!",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+                Map<String, String> params = new HashMap<>();
+                params.put("mobile_number",mobile_number.getText().toString().trim());
+                return params;
+            }
+        };
+        requestQueue.add(stringRequest);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //        mobile_number = findViewById(R.id.mobile_number);
 //        error = findViewById(R.id.tv_numbernotfound);
 //        et_Otp = findViewById(R.id.et_otp);
@@ -178,4 +296,4 @@ Button  btn_reset , btn_otpVerification , send_otp;
 //        requestQueue.add(stringRequest);
     }
 
-}
+
--- app/src/main/java/com/spyromedia/android/kamvia/PasswordResetActivity.java
+++ app/src/main/java/com/spyromedia/android/kamvia/PasswordResetActivity.java
@@ -1,14 +1,146 @@
 package com.spyromedia.android.kamvia;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class PasswordResetActivity extends AppCompatActivity {
+
+    TextInputEditText password_text,confirm_password_text;
+    MaterialButton password_verify_btn;
+    String password , confirmpassword;
+    String mobile_number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_reset);
+        password_text = findViewById(R.id.password_reset_text);
+        confirm_password_text = findViewById(R.id.confirm_password_text);
+        password_verify_btn = findViewById(R.id.password_verify_btn);
+        Intent intent = getIntent();
+        mobile_number = intent.getStringExtra("mobile_number");
+        Log.d("passwordreset", "o"+mobile_number);
+
+
+        password_verify_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(verify())
+                {
+                    passwordReset();
+                }
+                else {
+                    Toast.makeText(PasswordResetActivity.this, "Password reset failed", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
+    }
+
+    private void passwordReset() {
+        String url = "http://18.220.53.162/kamvia/api/ResetPassword.php";
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+       StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+           @Override
+           public void onResponse(String response) {
+
+               try {
+                    JSONObject jsonObject = new JSONObject(response);
+
+                   Log.d("PasswordReset", "onResponse: "+jsonObject.getString("message"));
+
+                   if(jsonObject.getString("message").equals("Password changed"))
+                   {
+                       Toast.makeText(PasswordResetActivity.this, "Password Changed", Toast.LENGTH_SHORT).show();
+                       startActivity(new Intent(PasswordResetActivity.this,LoginActivity.class));
+                   }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+
+               }
+
+           }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+               if (error instanceof NetworkError) {
+                } else if (error instanceof ServerError) {
+
+                    Toast.makeText(PasswordResetActivity.this, "Server Error"+error, Toast.LENGTH_SHORT).show();
+
+                } else if (error instanceof AuthFailureError) {
+                } else if (error instanceof ParseError) {
+                } else if (error instanceof NoConnectionError) {
+                } else if (error instanceof TimeoutError) {
+                    Toast.makeText(PasswordResetActivity.this,
+                            "Oops. Timeout error!",
+                            Toast.LENGTH_LONG).show();
+               }
+            }
+
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+               Map<String, String> params = new HashMap<>();
+                params.put("password", password_text.getText().toString().trim());
+                params.put("mobile_number",mobile_number);
+                return params;
+           }
+        };
+        requestQueue.add(stringRequest);
+    }
+
+
+
+
+    private Boolean verify() {
+        if (password_text.getText().toString().isEmpty()) {
+
+           password_text.setError("Please enter a password");
+            return false;
+       }
+       if (confirm_password_text.getText().toString().isEmpty()) {
+           confirm_password_text.setError("Please enter a password");
+           return false;        }
+       if (!password_text.getText().toString().equals(confirm_password_text.getText().toString())) {
+           confirm_password_text.setError("Passwords not matching");
+            return false;
+       }
+
+        return true;
     }
 }
