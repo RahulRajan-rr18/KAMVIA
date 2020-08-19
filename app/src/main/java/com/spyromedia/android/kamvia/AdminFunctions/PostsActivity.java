98a208bed0b4e9903d540be07af339e76006e264
patrickprakash <patrickprakash8@gmail.com>
2020-08-20 00:30:08 +0530

    Some UI Changes

--- app/src/main/java/com/spyromedia/android/kamvia/AdminFunctions/PostsActivity.java
+++ app/src/main/java/com/spyromedia/android/kamvia/AdminFunctions/PostsActivity.java
@@ -1,11 +1,11 @@
 package com.spyromedia.android.kamvia.AdminFunctions;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.spyromedia.android.kamvia.AddOrdersOrCirculars;
 import com.spyromedia.android.kamvia.R;
@@ -17,7 +17,7 @@ public class PostsActivity extends AppCompatActivity implements View.OnClickList
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_posts);
+        setContentView(R.layout.activity_posts_new);
         getSupportActionBar().hide();
 
 
